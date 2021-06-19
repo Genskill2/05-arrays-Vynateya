97e01d9465e61280ac6fc567eac25b3e68c55eb9
Vynateya <84716564+Vynateya@users.noreply.github.com>
2021-06-19 13:31:09 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -20,3 +20,27 @@ int main(void) {
   float average=sum/j;
   return average;
   }
+
+#include <stdio.h>
+#include <assert.h>
+
+
+int max(int [], int);
+
+int main(void) {
+  int x[] = {9,5,6,10,2,-3,4};
+  assert (max(x, 7) == 10);
+
+  int y[] = {5};
+  assert (max(y, 1) == 5);
+  printf("Max: passed\n");
+  }
+  int max(int x[], int j)
+  {
+  int i;
+    int max = x[0];
+    for (i = 1; i < j; i++)
+        if (x[i] > max)
+            max = x[i];
+            return max;
+  }
